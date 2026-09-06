c54aa54f9fc127431586b4ab7cce215bb0a1ce09
nicolasbuelvas <1011089074@u.icesi.edu.co>
2026-09-06 17:25:51 -0500

    feat(repository): implement TrackRepository with spring annotation @Repository

---
+++ demo/src/main/java/repository/TrackRepository.java
@@ -0,0 +1,38 @@
+package repository;
+
+import model.Track;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TrackRepository {
+    
+    private List<Track> tracks = new ArrayList<>();
+
+    public void addTrack(Track track) {
+        tracks.add(track);
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public Track getTrackById(String id) {
+        if (id == null) return null;
+        for (Track track : tracks) {
+            if (track.getId().equalsIgnoreCase(id.trim())) {
+                return track;
+            }
+        }
+        return null;
+    }
+
+    public boolean removeById(String id) {
+        if (id == null) return false;
+        return tracks.removeIf(track -> track.getId().equalsIgnoreCase(id.trim()));
+    }
+
+}
